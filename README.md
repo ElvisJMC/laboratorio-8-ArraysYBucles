c1fb0b498980b0b117bc7f86a9666de5201328f0
Elvis <elvismendez91@gmail.com>
2025-02-11 19:48:21 +0100

    Ejercicio 8.1 terminado

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Laboratorio-2-Tipos de datos y operadores.
-
-- En un principio había hecho cada cáculo de manera separada, pero después de practicar e investigar un poco he visto que se podía hacer todo el cálculo junto.
+# Laboratorio 8.1 bucles
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,213 @@
-import "./style.css";
+type Especialidad = "Medico de familia" | "Pediatra" | "Cardiólogo";
 
-console.log((120 - 18) / 6);
-console.log("Cada comensal tiene que pagar 17€ por lo cena, sin tener en cuenta las bebidas");
+interface Pacientes {
+  id: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  temperatura: number;
+  frecuenciaCardiaca: number;
+  especialidad: Especialidad;
+  edad: number;
+}
 
-console.log((120 - 18) / 6 + 18);
-console.log("El cumpleañero tiene que pagar 35€, ya que aparte de pagar su parte correspondiente de la cena, también invita a las bebidas.");
+const pacientes: Pacientes[] = [
+  {
+    id: 1,
+    nombre: "John",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Medico de familia",
+    edad: 44,
+  },
+  {
+    id: 2,
+    nombre: "Jane",
+    apellidos: "Doe",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 70,
+    especialidad: "Medico de familia",
+    edad: 43,
+  },
+  {
+    id: 3,
+    nombre: "Junior",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 90,
+    especialidad: "Pediatra",
+    edad: 8,
+  },
+  {
+    id: 4,
+    nombre: "Mary",
+    apellidos: "Wien",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 120,
+    especialidad: "Medico de familia",
+    edad: 20,
+  },
+  {
+    id: 5,
+    nombre: "Scarlett",
+    apellidos: "Somez",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 110,
+    especialidad: "Cardiólogo",
+    edad: 30,
+  },
+  {
+    id: 6,
+    nombre: "Brian",
+    apellidos: "Kid",
+    sexo: "Male",
+    temperatura: 39.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Pediatra",
+    edad: 11,
+  },
+];
+
+// Apartado1:
+
+//Ejercicio A.
+const obtenPacientesAsignadosAPediatria = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const pacientesAsignadosAPediatria = [];
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra") {
+      pacientesAsignadosAPediatria.push(pacientes[i]);
+    }
+  }
+  return pacientesAsignadosAPediatria;
+};
+
+console.log(
+  "Pacientes Asignados a pediatría:",
+  obtenPacientesAsignadosAPediatria(pacientes)
+);
+
+// Ejercicio B.
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const pacientesEnPediatriaMenoresDe10Anios = [];
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra" && pacientes[i].edad < 10) {
+      pacientesEnPediatriaMenoresDe10Anios.push(pacientes[i]);
+    }
+  }
+  return pacientesEnPediatriaMenoresDe10Anios;
+};
+
+console.log(
+  "Pacientes asignados a padiatria menores de 10 años:",
+  obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes)
+);
+
+//Apartado2:
+
+const activarProtocoloDeUrgencia = (pacientes: Pacientes[]): boolean => {
+  let activarProtocolo = false;
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (
+      pacientes[i].frecuenciaCardiaca > 100 &&
+      pacientes[i].temperatura > 39
+    ) {
+      activarProtocolo = true;
+      break;
+    }
+  }
+  return activarProtocolo;
+};
+
+console.log(
+  "Protoclo de emergencia activado:",
+  activarProtocoloDeUrgencia(pacientes)
+);
+
+// Apartado 3:
+
+const reasignaPacientesAMedicoFamilia = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const pacientesReasignados = [];
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra") {
+      let pacientesModificados = {
+        ...pacientes[i],
+        especialidad: "Medico de familia" as Especialidad,
+      };
+      pacientesReasignados.push(pacientesModificados);
+    }
+  }
+  return pacientesReasignados;
+};
+
+console.log(
+  "Pacientes reasignados de pediatría a médico de familia:",
+  reasignaPacientesAMedicoFamilia(pacientes)
+);
+
+// Apartado 4
+
+const hayPacientesDePediatria = (pacientes: Pacientes[]): boolean => {
+  let pacientesEnPediatria = false;
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra") {
+      pacientesEnPediatria = true;
+      break;
+    }
+  }
+  return pacientesEnPediatria;
+};
+
+console.log(
+  "¿Hay pacientes en Pediatría?:",
+  hayPacientesDePediatria(pacientes)
+);
+
+// Apartado 5:
+
+interface NumeroPacientesPorEspecialidad {
+  medicoDeFamilia: number;
+  pediatria: number;
+  cardiologia: number;
+}
+
+const cuentaPacientesPorEspecialidad = (
+  pacientes: Pacientes[]
+): NumeroPacientesPorEspecialidad => {
+  let totalPacientesPorEspecialidad: NumeroPacientesPorEspecialidad = {
+    medicoDeFamilia: 0,
+    pediatria: 0,
+    cardiologia: 0,
+  };
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Medico de familia") {
+      totalPacientesPorEspecialidad.medicoDeFamilia++;
+    } else if (pacientes[i].especialidad === "Pediatra") {
+      totalPacientesPorEspecialidad.pediatria++;
+    } else if (pacientes[i].especialidad === "Cardiólogo") {
+      totalPacientesPorEspecialidad.cardiologia++;
+    }
+  }
+  return totalPacientesPorEspecialidad;
+};
+
+console.log(
+  "Número total de pacientes por especialidad:",
+  cuentaPacientesPorEspecialidad(pacientes)
+);
